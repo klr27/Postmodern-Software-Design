0dc49d3f1e1a2d5609f87ed8fa67ead8a19ba52a
Kali Ruppert <kruppert@smu.edu>
2015-09-14 11:33:39 -0500

    Implement beginnings of XMLHttpRequest

--- onePage.php
+++ onePage.php
@@ -9,6 +9,20 @@
   <link type="text/css" rel="stylesheet" href="Project01/index.css"/>
   <title>Top 100 Albums of All Time</title>
   <meta name="description" context="A list of the top 100 albums of all time, according to Rolling Stone."/>
+   <script>
+  function replaceList() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status == 200){
+        document.getElementById("table").innerHTML  = xhr.responseText;
+      }
+    }
+    xhr.open("POST", "http://kruppert.humanoriented.com/table.php", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+    var sortOrder = document.getElementByID("topAlbums"),value;
+    xhr.send("sortOrder" + sortOrder);
+  }
+  </script>
 </head>
 
 <body>
@@ -23,39 +37,11 @@
           <option <? if ($sortOrder == "rank") { ?>selected = "selected"<? } ?>value="rank">Rank</option>
           <option <? if ($sortOrder == "year") { ?>selected = "selected"<? } ?>value="year">Year</option>
           <option <? if ($sortOrder == "title") { ?>selected = "selected"<? } ?>value="title">Title</option>
-        <input type="submit" value="Sort!"/>
+        <input type="submit" onclick="replaceList(); return false;"/> 
         </select>
       </form>
-
- <table>
-  <thead>
-    <tr>
-      <td>Rank</td>
-      <td>Title</td>
-      <td>Year</td>
- </tr>
-  </thead>
-<?php
-$servername = "crcp3320db.humanoriented.com";
-$username = "crcp3320";
-$password = "Crcp3320";
-$dbname = "crcp3320";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$sql = "SELECT * FROM albums ORDER BY " . $sortOrder . ";";
- 
-$result = $conn->query($sql);
-while($row = $result->fetch_assoc()) {
-    echo "<tr>\n" . "\t<td>" . $row["rank"] . "</td>\n<td>" . $row["title"]. "</td>\n<td>" . $row["year"]. "</td>\n</tr>\n";
-}
-$conn->close();
-?>
-
-</table>
+<div id="table">
+</div>      
 </body>
 </html>
 
